f295e9cfde1dc8d0e71df0046ca7e25ec12200a0
KenedyBorba <kenedy@rede.ulbra.br>
2020-06-22 13:54:50 -0300

    nvs arquivos

--- atividade 5 - js/script.js
+++ atividade 5 - js/script.js
@@ -1,19 +1,21 @@
 function VerificarImc() {
     var altura = parseFloat(document.getElementById("altura").value);
     var peso = parseFloat(document.getElementById("peso").value);
+    var nome = document.getElementById("nome").value
 
     var imc = peso / (altura * altura);
+    var imc = imc.toFixed(2);
     if (imc < 18.5) {
-        resultado = "Abaixo do peso ideal";
+        resultado = nome + ", seu IMC é de " + imc + " e você está abaixo do peso ideal";
     }
     if (imc >= 18.5 && imc < 25) {
-        resultado = "No peso ideal";
+        resultado = nome + ", seu IMC é de " + imc + " e você está no peso ideal";
     }
     if (imc >= 25 && imc <= 30) {
-        resultado = "Acima do peso ideal";
+        resultado = nome + ", seu IMC é de " + imc + " e você está acima do peso ideal";
     }
     if (imc > 30) {
-        resultado = "Obeso";
+        resultado = nome + ", seu IMC é de " + imc + " e você está obeso(a)";
     }
 
     document.getElementById("result").innerHTML = resultado;
