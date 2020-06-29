ba09acb921fc31d4a5bbc307c3b575c7807486b3
KenedyBorba <kenedy@rede.ulbra.br>
2020-06-28 23:43:02 -0300

    nvs

--- AS/script.js
+++ AS/script.js
@@ -1,15 +1,20 @@
 function Verificar() {
-    var nota1 = parseInt(document.getElementById("nota1").value);
-    var nota2 = parseInt(document.getElementById("nota2").value);
-    var nota3 = parseInt(document.getElementById("nota3").value);
-    var nota4 = parseInt(document.getElementById("nota4").value);
-    var nota5 = parseInt(document.getElementById("nota5").value);
-    var nota6 = parseInt(document.getElementById("nota6").value);
+    var nota1 = parseFloat(document.getElementById("nota1").value.replace(/\D/g, ""));
+    var nota2 = parseFloat(document.getElementById("nota2").value.replace(/\D/g, ""));
+    var nota3 = parseFloat(document.getElementById("nota3").value.replace(/\D/g, ""));
+    var nota4 = parseFloat(document.getElementById("nota4").value.replace(/\D/g, ""));
+    var nota5 = parseFloat(document.getElementById("nota5").value.replace(/\D/g, ""));
+    var nota6 = parseFloat(document.getElementById("nota6").value.replace(/\D/g, ""));
 
     var media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6;
     var media = media.toFixed(2);
-    resultado = "De acordo com as notas que você deu para cada cena a média é " + media;
-
+    if (media >= 0 && media <= 10) {
+        resultado = "De acordo com as notas que você deu para cada cena a média é " + media;
+    }
+    else {
+        resultado = "Ocorreu algum erro."
+    }
 
     document.getElementById("result").innerHTML = resultado;
+    return false
 }
