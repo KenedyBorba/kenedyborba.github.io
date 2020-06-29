59a0ad6b6d15c4eaa3aebcec5f14b2ae57a57a3c
KenedyBorba <kenedy@rede.ulbra.br>
2020-06-29 00:34:40 -0300

    nvs arquivos

--- AS/script.js
+++ AS/script.js
@@ -1,10 +1,10 @@
 function Verificar() {
-    var nota1 = parseFloat(document.getElementById("nota1").value.replace(/\D/g, ""));
-    var nota2 = parseFloat(document.getElementById("nota2").value.replace(/\D/g, ""));
-    var nota3 = parseFloat(document.getElementById("nota3").value.replace(/\D/g, ""));
-    var nota4 = parseFloat(document.getElementById("nota4").value.replace(/\D/g, ""));
-    var nota5 = parseFloat(document.getElementById("nota5").value.replace(/\D/g, ""));
-    var nota6 = parseFloat(document.getElementById("nota6").value.replace(/\D/g, ""));
+    var nota1 = parseFloat(document.getElementById("nota1").value);
+    var nota2 = parseFloat(document.getElementById("nota2").value);
+    var nota3 = parseFloat(document.getElementById("nota3").value);
+    var nota4 = parseFloat(document.getElementById("nota4").value);
+    var nota5 = parseFloat(document.getElementById("nota5").value);
+    var nota6 = parseFloat(document.getElementById("nota6").value);
 
     var media = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6) / 6;
     var media = media.toFixed(2);
@@ -16,5 +16,4 @@ function Verificar() {
     }
 
     document.getElementById("result").innerHTML = resultado;
-    return false
 }
